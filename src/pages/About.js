80e0d89bea270eb4654c8dff0098c791efbbb9d9
Min-92 <0201.mj.lee@gmail.com>
2020-06-01 15:51:49 +0900

    feat : query string 추가

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,15 @@
 import React from "react";
+import queryString from "query-string";
 
-const About = ({ match }) => {
+const About = ({ match, location }) => {
+  const query = queryString.parse(location.search);
+
+  const detail = query.detail === "true";
+
   return (
     <div>
       <h2>About {match.params.name}</h2>
+      {detail && `detail page`}
     </div>
   );
 };
