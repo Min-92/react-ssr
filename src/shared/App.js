d8af6340bb186a8056a028c13d547429987faa4b
Min-92 <0201.mj.lee@gmail.com>
2020-06-01 15:44:12 +0900

    refactor : switch 적용

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Home, About } from "pages";
 class App extends Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route path="/about/:name" component={About} />
+        <Switch>
+          <Route path="/about/:name" component={About} />
+          <Route path="/about" component={About} />
+        </Switch>
       </div>
     );
   }
